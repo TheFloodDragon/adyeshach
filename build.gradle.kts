a428de9d1a3b27963d822c7572ec476fbb970674
MC~蛟龙 <1610105206@qq.com>
2025-02-08 09:58:25 +0800

    CI: github workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     java
     id("io.izzel.taboolib") version "2.0.19" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.8.22" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.1.10" apply false
 }
 
 subprojects {
